7e29650ecc3fffab9fab8854ee035b3fe110960d
Nirupam7423 <nirupamatiitd@gmail.com>
2024-05-26 12:19:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/api", (req, res) => {
+  res.send({ message: "Hello from the Node.js server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
